6c28b3706a9d58d9b5c494f51f83b6956b144419
Mauro Castagnasso <mauro.castagnasso@gmail.com>
2017-04-30 21:31:56 -0300

    Adds program to normalize the records of the dataset generated by the automated player.

---
+++ programs/normalizer/Normalizer/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Dataset Normalizer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Grupo 1")]
+[assembly: AssemblyProduct("Dataset Normalizer")]
+[assembly: AssemblyCopyright("Copyright © Grupo 1 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("16ba5a79-e6a2-42d1-a366-0ecf8262e1e6")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
