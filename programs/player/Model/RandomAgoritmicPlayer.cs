cea53a1b580d138ec0063602ca6abf939d926f33
Santiago Federella <santiago.federella@outlook.com>
2017-05-15 13:35:04 -0300

    Update and rename StupidAgoritmicPlayer.cs to RandomAgoritmicPlayer.cs

--- programs/player/Model/RandomAgoritmicPlayer.cs
+++ programs/player/Model/RandomAgoritmicPlayer.cs
@@ -6,19 +6,19 @@ using System.Threading.Tasks;
 
 namespace connect_4_ai.Model
 {
-    class StupidAlgoritmicPlayer : AlgoritmicPlayer
+    class RandomAlgoritmicPlayer : AlgoritmicPlayer
     {
-        private double stupidity;
+        private double randomChance;
 
-        public StupidAlgoritmicPlayer(int maxDepth, string resultsPath, double stupidity) : base(maxDepth, resultsPath)
+        public StupidAlgoritmicPlayer(int maxDepth, string resultsPath, double randomChance) : base(maxDepth, resultsPath)
         {
-            this.stupidity = (stupidity > 1 ? 1 : (stupidity < 0 ? 0 : stupidity));
+            this.randomChance = (randomChance > 1 ? 1 : (randomChance < 0 ? 0 : randomChance));
         }
 
         public override int getSelectedCol(Board board)
         {
             Random random = new Random();
-            if (stupidity > random.NextDouble())
+            if (randomChance > random.NextDouble())
             {
                 List<int> colsWithSpace = new List<int>();
                 for (int col=0; col<board.columns; col++)
